7539b8a5a956777e56725fec493108865cc1af20
Pedro Henrique Palhano Modolo <modolo.ph@gmail.com>
2025-02-09 14:44:03 -0500

    Initial Commit

---
+++ PKUFuel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PKUFuel
+//
+//  Created by Pedro Henrique Palhano Modolo on 2/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PKUFuel/PKUFuelApp.swift
@@ -0,0 +1,17 @@
+//
+//  PKUFuelApp.swift
+//  PKUFuel
+//
+//  Created by Pedro Henrique Palhano Modolo on 2/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PKUFuelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
